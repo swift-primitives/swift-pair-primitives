e90d28df96a102a43c4c061c32491784182efc73
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 09:56:56 +0200

    Adopt Swift 6.4 package baseline

--- Package.swift
+++ Package.swift
@@ -1,26 +1,35 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-pair-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
             name: "Pair Primitives",
             targets: ["Pair Primitives"]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-equation-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-hash-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-comparison-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-equation-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-hash-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-comparison-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -34,7 +43,7 @@ let package = Package(
         .testTarget(
             name: "Pair Primitives Tests",
             dependencies: [
-                "Pair Primitives",
+                "Pair Primitives"
             ]
         ),
     ],
